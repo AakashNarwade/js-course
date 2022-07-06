cec4193295c440a61e8101ff99a7a1677a132f69
AakashNarwade <aakashnarwade06@gmail.com>
2022-07-06 14:25:19 +0530

    In use strict  mode the scope of the function is block scope , but if you remove the use strict mode then it can run

--- starterbhs/script.js
+++ starterbhs/script.js
@@ -1,27 +1,30 @@
 'use strict';
 
 function calcAge(birthYear) {
-    
-    const age = 2022 - birthYear;
-    console.log(firstName);
+  const age = 2022 - birthYear;
+  console.log(firstName);
 
-    function printAge(){
-        const outputAge = `${firstName} You are ${age}, born in ${birthYear}`;
-        console.log(outputAge);
-        var millenial = true;
-        if (birthYear >=1981 && birthYear <=1996) {
-            const str= `Ohh ohh you are a millenial ${firstName}`
-        }
-        // console.log(str);
-        console.log(millenial);
-        // var is function scope thats why it ignores the block  scope and search for variable value upwards in function block .
+  function printAge() {
+    const outputAge = `${firstName} You are ${age}, born in ${birthYear}`;
+    console.log(outputAge);
+    var millenial = true;
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Ohh ohh you are a millenial ${firstName}`;
+      function sum(a, b) {
+        return a + b;
+      }
     }
-    printAge();
-   
-    return age;
+    // console.log(str);
+    console.log(millenial);
+    // var is function scope thats why it ignores the block  scope and search for variable value upwards in function block .
+
+    console.log(sum(5, 6));
+    // In use strict  mode the scope of the function is block scope , but if you remove the use strict mode then it can run
+  }
+  printAge();
+
+  return age;
 }
 
-const firstName = "aakash";
+const firstName = 'aakash';
 calcAge(1996);
-
-
