f86e421069005402b1ee22eff7ac4e358b3d1e55
AakashNarwade <aakashnarwade06@gmail.com>
2022-07-06 16:16:01 +0530

    Hoisting in variables

--- starterbhs/script.js
+++ starterbhs/script.js
@@ -1,30 +1,37 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2022 - birthYear;
-  console.log(firstName);
+// function calcAge(birthYear) {
+//   const age = 2022 - birthYear;
+//   console.log(firstName);
 
-  function printAge() {
-    const outputAge = `${firstName} You are ${age}, born in ${birthYear}`;
-    console.log(outputAge);
-    var millenial = true;
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      const str = `Ohh ohh you are a millenial ${firstName}`;
-      function sum(a, b) {
-        return a + b;
-      }
-    }
-    // console.log(str);
-    console.log(millenial);
-    // var is function scope thats why it ignores the block  scope and search for variable value upwards in function block .
+//   function printAge() {
+//     const outputAge = `${firstName} You are ${age}, born in ${birthYear}`;
+//     console.log(outputAge);
+//     var millenial = true;
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       const str = `Ohh ohh you are a millenial ${firstName}`;
+//       function sum(a, b) {
+//         return a + b;
+//       }
+//     }
+//     // console.log(str);
+//     console.log(millenial);
+//     // var is function scope thats why it ignores the block  scope and search for variable value upwards in function block .
 
-    console.log(sum(5, 6));
-    // In use strict  mode the scope of the function is block scope , but if you remove the use strict mode then it can run
-  }
-  printAge();
+//     console.log(sum(5, 6));
+//     // In use strict  mode the scope of the function is block scope , but if you remove the use strict mode then it can run
+//   }
+//   printAge();
 
-  return age;
-}
+//   return age;
+// }
 
-const firstName = 'aakash';
-calcAge(1996);
+// const firstName = 'aakash';
+// calcAge(1996);
+
+// Hoisting in variables
+console.log(me); //undefined
+console.log(job);
+var me = 'aakash';
+let job = 'software engineer';
+const year = 1996;
